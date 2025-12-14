d4c6ab2d8fb36b8020f77e47eaccff062f4a6314
SumitSingh008 <sumitsingh0216@gmail.com>
2025-12-14 22:52:41 +0530

    ...

--- src/index.js
+++ src/index.js
@@ -1,5 +1,6 @@
 import dotenv from "dotenv";
 import connectDB from "./db/index.js";
+import app from "./app.js";
 
 dotenv.config({
     path: './.env'
@@ -14,8 +15,6 @@ connectDB()
 .catch((err) => {
     console.log("MONGO db connection failed !!!", err);
 })
-
-
 
 
 /*
